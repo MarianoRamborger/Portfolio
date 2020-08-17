9d6317e39c57458bb7bf65c8ec05ba1d66241ecb
MarianoRamborger <Marianoramborger@gmail.com>
2020-08-17 11:51:49 -0300

    bug fixed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import Start from './Sections//Start/Start'
 import RprojectsDivider from './Sections/RecentProjects/RProjectsDivider'
 import Rproject from './Sections/RecentProjects/RProject'
 import Footer from './Sections/Footer/Footer'
+
 import {Ping} from './Services/Ping'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Redirect,
+  //Link
+} from "react-router-dom";
 
 export const LanguageContext = React.createContext();
 
@@ -21,31 +29,40 @@ const App = () => {
     Ping()
   })
 
-    console.log(language)
   
   return (
     
-    <React.Fragment>
-
-      <LanguageContext.Provider value={{language, changeLanguage}}> 
-      {/* Context union point */}
-
-      <Start language={language} changeLanguage={changeLanguage}/>
-
-      <RprojectsDivider />
-        <Rproject template="orange" language={language}  />
-        <Rproject template="red"  language={language}/>
-        <Rproject template="blue"  language={language}/>
-
-      <Footer language={language} />
-
-      <div className="soil" 
-        
-      > </div>
+    <Router>
+      <Switch>
+      
+        <Route>
+        <React.Fragment>
 
 
-      </LanguageContext.Provider>
-    </React.Fragment>
+            <LanguageContext.Provider value={{language, changeLanguage}}> 
+            {/* Context union point */}
+
+            <Start language={language} changeLanguage={changeLanguage}/>
+
+            <RprojectsDivider />
+              <Rproject template="orange" language={language}  />
+              <Rproject template="red"  language={language}/>
+              <Rproject template="blue"  language={language}/>
+
+            <Footer language={language} />
+
+            <div className="soil" 
+              
+            > </div>
+
+
+            </LanguageContext.Provider>
+          </React.Fragment>
+
+        </Route>
+
+      </Switch>
+    </Router>
 
 
   )
